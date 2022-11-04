505b625914f5c0938601f7c9ce47e3ee816da995
first-p <98608650+first-p@users.noreply.github.com>
2022-11-04 16:49:58 -0600

    structure of hastable class complete

--- hashtable.cpp
+++ hashtable.cpp
@@ -5,3 +5,40 @@ Purpose: hashtable.cpp is the hash table class
 and includes all operations for manipulating a
 hash table object
 *********************/
+
+#include "hashtable.h"
+
+HashTable::HashTable(){
+
+}
+
+HashTable::~HashTable(){
+
+}
+
+bool HashTable::insertEntry(int id, string *data){
+
+}
+string HashTable::getData(int id){
+
+}
+
+
+bool HashTable::removeEntry(int id){
+
+}
+
+int HashTable::getCount(){
+
+}
+
+void HashTable::printTable(){
+
+}
+
+
+int HashTable::hast(int id){
+
+}
+
+
