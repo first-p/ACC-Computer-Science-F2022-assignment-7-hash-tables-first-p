2b9c78865a4d03ae9e46bf25620c5bfb9b8ed168
first-p <98608650+first-p@users.noreply.github.com>
2022-11-06 17:19:15 -0700

    formatting

--- linkedList.h
+++ linkedList.h
@@ -1,12 +1,12 @@
 /*********************
- Name: Fred Butoma
- Assignment 7
- Purpose: linkedlist.h has all the prototype
- methods for the linked list class 
- *********************/
+Name: Fred Butoma
+Assignment 6
+Purpose: linkedlist.h has all the prototype
+methods for the linked list class 
+*********************/
 
- #ifndef LINKED_LIST_H
- #define LINKED_LIST_H
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
 
 #include "data.h"
 #include <iostream>
@@ -15,34 +15,39 @@ using std::cout;
 using std::endl;
 
 class LinkedList {
-    public:
-        // constructor
-        LinkedList();
-        // destructor
-        ~LinkedList();
-        // getters
-        bool getNode(int, Data *);
-        void printList(bool = false);
-        int getCount();
-        bool exists(int);
-        // setters
-        bool addNode(int, string *);
-        bool deleteNode(int);
-        bool clearList();
 
-    private:
-        // attributes
-        Node *head;
-        // helper methods
-        bool addHead(int, string *);
-        bool delHead(Node*);
-        bool addTail(int, string *, Node *);
-        bool delTail(Node*);
-        bool addMiddle(int, string *, Node *);
-        bool delMiddle(Node*);
-        bool isLast(Node*);
-        bool delLast(Node*);
-        Node *createNode(int, string *);
+public:
+  // constructor
+  LinkedList();
+
+  // destructor
+  ~LinkedList();
+
+  // getters
+  bool getNode(int, Data *);
+  void printList(bool = false);
+  int getCount();
+  bool exists(int);
+
+  // setters
+  bool addNode(int, string *);
+  bool deleteNode(int);
+  bool clearList();
+
+private:
+  // attributes
+  Node *head;
+  // helper methods
+  bool addHead(Node*);
+  bool delHead(Node*);
+  bool addTail(Node*, Node *);
+  bool delTail(Node*);
+  bool addMiddle(Node*, Node *);
+  bool delMiddle(Node*);
+  bool isLast(Node*);
+  bool delLast(Node*);
+  Node *createNode(int, string *);
 };
 
 #endif // LINKED_LIST_H
+
